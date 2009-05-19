b14206a16dbd2344318ea26ef898c15446d76318
pierre <pierre@uneek-pierre.(none)>
2009-05-19 13:54:03 +0200

    add all file to start extensions

---
+++ tinymce_filter_extension.rb
@@ -0,0 +1,21 @@
+class TinymceFilterExtension < Radiant::Extension
+  version "1.0"
+  description "Provides a TinyMCE filter for the Radiant CMS."
+  url "http://github.com/miv/radiant_tinymce_filter/tree/master"
+  
+  def activate
+    # Load the filter
+    TinyMceFilter
+    
+    
+    admin.page.edit.add :form, "/admin/shared/js"
+    admin.snippet.edit.add :form_top, "/admin/shared/js"
+    #admin.page.edit.add :part_controls, "/admin/page/editor_control"
+    #admin.snippet.edit.add :form_top, "/admin/page/editor_control"
+  end
+  
+  def deactivate
+#    admin.page.edit.remove :part_controls
+  end
+    
+end
